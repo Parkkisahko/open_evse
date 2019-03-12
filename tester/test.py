9a818a00981a395ac58e426bcf0fa96cf3dbd1ee
santerioksanen <santeri.oksanen@gmail.com>
2019-03-12 13:32:18 +0200

    Test cases against default open_evse

--- tester/test.py
+++ tester/test.py
@@ -18,11 +18,17 @@ class OpenEVSETester(unittest.TestCase):
 
     def setEVSECurrent(self, current):
         self.serial_open_evse.write(bytes('$SC {}\r\n'.format(current), 'ascii'))
+        #print(self.serial_open_evse.readline())
     
-    def initEVSE(self):
+    def initEVSE(self, current):
         self.serial_open_evse.write(b'$FF D 0') # disable diode chec
+        #print(self.serial_open_evse.readline())
         time.sleep(0.5)
         self.serial_open_evse.write(b'$FF G 0') # disable ground check
+        #print(self.serial_open_evse.readline())
+        time.sleep(0.5)
+        self.serial_open_evse.write(b'$SL 2')   # Set service level 2
+        #print(self.serial_open_evse.readline())
         time.sleep(0.5)
         self.setEVSECurrent(6)
         time.sleep(0.5)
@@ -31,7 +37,14 @@ class OpenEVSETester(unittest.TestCase):
         self.assertTrue(980 <= meas['freq'] <= 1020, 'Frequency out of bounds: {} Hz'.format(meas['freq']))
 
     def assert_PWM(self, pwm, amps):
-        self.assertTrue(9.1 <= pwm/100.0 <= 11.1, 'PWM out of bounds, duty cycle: {} %'.format(round(pwm/100.0, 1)))
+        if amps == 0:
+            cycle = 0
+        elif amps <= 51:
+            cycle = amps / 0.6
+        else:
+            cycle = amps / 2.5 + 64
+
+        self.assertTrue(cycle - 1.2 <= pwm/100.0 <= cycle + 1.2, 'PWM out of bounds, duty cycle: {} %'.format(round(pwm/100.0, 1)))
 
     def setUp(self):
         self.serial_open_evse = serial.Serial(OPEN_EVSE_PORT, baud)
@@ -43,9 +56,20 @@ class OpenEVSETester(unittest.TestCase):
         self.setEVState('A')
         time.sleep(0.1)
         self.setTesterStateSend('0')
-        self.initEVSE()
-        self.serial_tester.reset_input_buffer()
     
+    def tearDown(self):
+        self.setTesterStateSend('0')
+        time.sleep(0.1)
+        self.setEVState('A')
+        
+
+class PWMOnTests(OpenEVSETester):
+
+    def setUp(self):
+        super().setUp()
+        self.initEVSE(6)
+        self.serial_tester.reset_input_buffer()
+
     # Test starts with state A, relay should remain open
     def test_basic(self):
         time.sleep(0.5)
@@ -56,9 +80,11 @@ class OpenEVSETester(unittest.TestCase):
             self.assertEqual(incoming['relay'], 0, "Relay closed even when should be open")
             self.assertEqual(incoming['freq'], 0, "PWM generates frequency even if should not")
             self.assertEqual(incoming['state'], 0)
-    
+
     # Test going to state B (plugged in)
     def test_plug_in(self):
+        print()
+        print()
         print('Starting plugged in test')
         pwms = []
         time.sleep(0.5)
@@ -77,16 +103,51 @@ class OpenEVSETester(unittest.TestCase):
         self.assert_PWM(sum(pwms)/len(pwms), 6)
         print('Plugged in test finished with average PWM of: {} %'.format(round(sum(pwms)/len(pwms)/100.0, 1)))
     
+    # Test pwm duty cycle at several amps
+    def assert_pwm_amps(self, amps):
+        print()
+        print()
+        print('Starting pwm test with amps: {} A'.format(amps))
+        self.setEVSECurrent(amps)
+        time.sleep(0.5)
+        self.setEVState('C')
+        time.sleep(1)
+        pwms = []
+        self.setTesterStateSend('1')
+        for i in range(10):
+            incoming = json.loads(self.serial_tester.readline())
+            print(incoming)
+            self.assert_freq(incoming)
+            pwms.append(incoming['PWM'])
+        self.assert_PWM(sum(pwms)/len(pwms), amps)
+        print('PWM test finished with average PWM of: {} %'.format(round(sum(pwms)/len(pwms)/100.0, 1)))
+    
+    def test_12_A(self):
+        self.assert_pwm_amps(12)
+    
+    def test_18_A(self):
+        self.assert_pwm_amps(18)
+    
+    def test_30_A(self):
+        self.assert_pwm_amps(30)
+    
+    def test_63_A(self):
+        self.assert_pwm_amps(63)
+
     # Test instant start charging
     def test_relay_timing_start_charging(self):
+        print()
+        print()
         print("Starting relay close test")
         pwms = []
         time.sleep(0.5)
         self.setEVState('C')
         self.setTesterStateSend('1')
+        relay_changed = False
         first_ts = None
         freq_start_ts = None
         last_ts = None
+        relay_true_counter = 0
         while True:
             incoming = json.loads(self.serial_tester.readline())
             print(incoming)
@@ -97,8 +158,13 @@ class OpenEVSETester(unittest.TestCase):
                     freq_start_ts = incoming['time']
                 self.assert_freq(incoming)
                 pwms.append(incoming['PWM'])
-            if incoming['relay'] == 1:
+            if incoming['relay'] == 1 and not relay_changed:
                 last_ts = incoming['time']
+                relay_changed = True
+            if relay_changed:
+                self.assertEqual(incoming['relay'], 1)
+                relay_true_counter += 1
+            if relay_true_counter == 20:
                 break
         
         self.assert_PWM(sum(pwms)/len(pwms), 6)
@@ -107,6 +173,8 @@ class OpenEVSETester(unittest.TestCase):
     
     # Test relay off timing
     def assert_relay_release_change(self, new_state, max_time=100):
+        print()
+        print()
         print('Starting relay release test, changing state to: {}'.format(new_state))
         time.sleep(0.5)
         self.setEVState('C')
@@ -135,7 +203,7 @@ class OpenEVSETester(unittest.TestCase):
         self.assert_relay_release_change('A')
     
     def test_relay_release_to_b(self):
-        self.assert_relay_release_change('B', 3000)
+        self.assert_relay_release_change('B', 3000)     # 3 sec according to SAE J1772 - 2016, 100 ms according to IEC 61851
 
     
 if __name__ == '__main__':
